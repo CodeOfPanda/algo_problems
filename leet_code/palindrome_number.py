cb77a4576a7a1c1969b0af002a7bf0a684bf735d
CodeOfPanda <mlynnleigland@gmail.com>
2021-01-26 11:40:09 -0700

    adding solution to palindrome number problem

---
+++ leet_code/palindrome_number.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/palindrome-number/submissions/
+# 01/26/2021
+
+
+# given an int x
+#   if x == palindrome 
+#       return true if
+
+# an int is a palindrome when it reads the same backward as forward.
+# 121 == 121 is a palindrome -121 != 121- not a palindrome.
+
+
+def is_palindrome(x):
+    
+    string_x = str(abs(x))
+    reverse_x = string_x[::-1]
+    
+    if x < 0: return False
+    
+    if string_x == reverse_x: 
+        return True
+
+    return False
+
+
+print(is_palindrome(-101))
